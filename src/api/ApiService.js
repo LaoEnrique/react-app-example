b8b625a0e7f5d442f2fdb3cfc42c483d53be046e
LaoEnrique <enrikegl96@gmail.com>
2019-10-11 09:52:39 -0700

    bug fixed

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: "https://unsplash.com",
     headers: {
         Authorization: 
-        "Client-ID 3e831733061b40b9cb978a05f2dad122c7322c326bae7a809d85c428fefc9863"
+        "Client-ID {id}" //you need create a new application en unsplash developer, replace id by access key
     }
 })
